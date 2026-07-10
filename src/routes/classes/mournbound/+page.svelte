3ef1ffd2f40bb418cd1c04f7ea04c4eec1997d92
Toon van Berkel <toonvanberkel203@gmail.com>
2026-07-10 12:18:32 +0200

    Update +page.svelte

---
+++ src/routes/classes/mournbound/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import AvailabilityBadges from "$lib/components/AvailabilityBadges.svelte";
+</script>
+
+<AvailabilityBadges banned={['Party 1', 'Party 2', 'Party 3', 'Party 4', 'Party 5', 'Party 6']} />
